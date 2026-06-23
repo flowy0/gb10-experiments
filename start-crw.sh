758b079da23d1b766dd02afa55f4ba5558287931
g <gayletan@gmail.com>
2026-06-23 10:58:46 +0800

    remove crw (deployed elsewhere)

--- start-crw.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Start crw - Firecrawl-compatible web scraper
-# Runs on port 3002
-cd /opt/atom
-nohup /opt/atom/crw/target/release/crw serve --port 3002 > /tmp/crw.log 2>&1 &
-echo "crw started on port 3002 (PID: $!)"
